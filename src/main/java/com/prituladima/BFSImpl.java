53e015945ab725454d8d18ed9fa58cf39d5d0249
Mykola_Zakapko <mykola_zakapko@epam.com>
2019-11-25 18:16:26 +0200

    BSF test is passed

--- src/main/java/com/prituladima/BFSImpl.java
+++ src/main/java/com/prituladima/BFSImpl.java
@@ -4,12 +4,26 @@ import java.util.*;
 
 public class BFSImpl {
 
-    private static final int inf = (int)1e6;
+    private static final int inf = (int) 1e6;
 
     private boolean[] used = new boolean[inf];
 
-    public void bfs(int from, Map<Integer, Collection<Integer>> graph, List<Integer> ans){
+    public void bfs(int from, Map<Integer, Collection<Integer>> graph, List<Integer> ans) {
         // TODO: 13.10.2019 Task 2: Make tests work
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(from);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            if (!ans.contains(v)) {
+                ans.add(v);
+                Collection<Integer> edges = graph.get(v);
+                for (int edge : edges) {
+                    Optional<Integer> w = graph.get(v).stream().filter(e -> e.equals(edge)).findFirst();
+                    w.ifPresent(queue::add);
+                }
+            }
+        }
+
     }
 
 }
